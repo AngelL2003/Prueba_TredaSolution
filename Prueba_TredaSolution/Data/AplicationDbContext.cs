1569ce07ce831741c5721ca9fd1d0a79d5fe72df
Angel2003 <4ngul012@gmail.com>
2023-05-15 01:18:16 -0500

    Se agrega el proyecto

---
+++ Prueba_TredaSolution/Data/AplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prueba_Tecnica_Treda.Entidades;
+
+
+namespace Prueba_TredaSolution.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        private readonly DbContextOptions<AplicationDbContext> options;
+
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+            this.options = options;
+        }
+
+
+        public DbSet<Tienda> Tiendas { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Tienda>().ToTable("Tienda");
+            //modelBuilder.Entity<Producto>().ToTable("Producto");
+
+            modelBuilder.Entity<Producto>().HasKey(p => p.SKU);
+
+            //Modifica la propiedad de FechaDeApertura de DateTime a Date
+            modelBuilder.Entity<Tienda>().Property(t => t.FechaDeApertura).HasColumnType("date");
+
+            //Modifica la propiedad de Imagen a obligatoria y sin caracteres unicode
+            modelBuilder.Entity<Producto>().Property(t => t.Imagen).IsRequired().IsUnicode(false);
+
+            //Modifica la propiedad de Valor para ser mas exacto con los decimales
+            modelBuilder.Entity<Producto>().Property(p => p.SKU).HasPrecision(18, 2);
+
+
+
+
+        }
+
+    }
+}
---
+++ Prueba_TredaSolution/Entidades/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Prueba_Tecnica_Treda.Entidades
+{
+    public class Producto
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public int SKU { get; set; }
+        [Required]
+        public string Descripcion { get; set; }
+        [Required]
+        public decimal Valor { get; set; }
+        [Required]
+        public string Imagen { get; set; }
+        [Required]
+        public int TiendaId { get; set; }
+        [Required]
+        public Tienda Tienda { get; set; }
+    }
+}
